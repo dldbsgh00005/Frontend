9ca6f6dd58a08bd77cf0c23937fb2ed0f9cb4b2f
dldbsgh00005 <dldbsgh00005@gmail.com>
2023-10-11 11:43:38 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes , Route } from 'react-router-dom';
+import Main from './Components/Main';
+import List from './Components/List';
+import Detail from './Components/Detail';
+
+function App() {
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
